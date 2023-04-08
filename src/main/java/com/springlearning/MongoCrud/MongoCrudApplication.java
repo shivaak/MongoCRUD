28bbcb4c90bd28a8e1a012c8a9ff6b43a33f7b2c
Shivaa Krishnan <shivainfotech12@gmail.com>
2023-04-08 15:02:32 +0530

    Add swagger ui support

--- src/main/java/com/springlearning/MongoCrud/MongoCrudApplication.java
+++ src/main/java/com/springlearning/MongoCrud/MongoCrudApplication.java
@@ -3,7 +3,17 @@ package com.springlearning.MongoCrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class MongoCrudApplication {
 
@@ -11,6 +21,21 @@ public class MongoCrudApplication {
 		SpringApplication.run(MongoCrudApplication.class, args);
 	}
 
+	@Bean
+	public Docket docket()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(generateApiInfo());
+	}
 
-
+	private ApiInfo generateApiInfo()
+	{
+		return new ApiInfoBuilder().title("MongoDB CRUD Operation Example").description("Sample MongoDB application using Spring Boot")
+				.version("Version 1.0").termsOfServiceUrl("urn:tos").contact(new Contact("Shivaa Krishnan", "https://github.com/shivaak", "shivainfotech12@gmail.com"))
+				.license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0").build();
+	}
 }
--- src/main/java/com/springlearning/MongoCrud/controller/ExpenseController.java
+++ src/main/java/com/springlearning/MongoCrud/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package com.springlearning.MongoCrud.controller;
 
 import com.springlearning.MongoCrud.model.Expense;
 import com.springlearning.MongoCrud.service.ExpenseService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +20,49 @@ public class ExpenseController {
         this.expenseService = expenseService;
     }
 
+    @ApiOperation(
+            value = "Add new expense",
+            notes = "Create new expense with expense name, category and amount",
+            produces = "application/json")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity addExpense(@RequestBody Expense expense) {
         expenseService.addExpense(expense);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(
+            value = "Update expense",
+            notes = "Update expense with new expense name, category and amount",
+            produces = "application/json")
     @PutMapping
     public ResponseEntity<Object> updateExpense(@RequestBody Expense expense) {
         expenseService.updateExpense(expense);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(
+            value = "Get all expense",
+            notes = "Provides list of all available expenses",
+            produces = "application/json")
     @GetMapping
     public ResponseEntity<List<Expense>> getAllExpenses() {
         return ResponseEntity.ok(expenseService.getAllExpenses());
     }
 
+
+    @ApiOperation(
+            value = "Get expense by name",
+            notes = "Filter the expense by name",
+            produces = "application/json")
     @GetMapping("/{name}")
     public ResponseEntity<Expense> getExpenseByName(@PathVariable String name) {
         return ResponseEntity.ok(expenseService.getExpenseByName(name));
     }
 
+    @ApiOperation(
+            value = "Delete expense by ID",
+            notes = "Delete the given expense",
+            produces = "application/json")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteExpense(@PathVariable  String id) {
         expenseService.deleteExpense(id);
